3f8a10352035e2d9bdcf84a1813e273b4a9e2811
josetamaritfernandez <al415487@uji.es>
2023-09-27 16:55:00 +0200

    clase 27/09

--- Teleoperation/scripts/launch_replay.bash
+++ Teleoperation/scripts/launch_replay.bash
@@ -2,4 +2,5 @@
 source /opt/ros/foxy/setup.bash
 export ROS_LOCALHOST_ONLY=1
 export TURTLEBOT3_MODEL=burger
-ros2 bag play rosbag2_2023_09_25-16_26_03
+ros2 bag play rosbag2_2023_09_27-16_31_26
+
--- Teleoperation/scripts/launch_rviz.bash
+++ Teleoperation/scripts/launch_rviz.bash
@@ -1,4 +1,4 @@
-	#!/bin/bash
+#!/bin/bash
 source /opt/ros/foxy/setup.bash
 export ROS_LOCALHOST_ONLY=1
 export TURTLEBOT3_MODEL=burger
