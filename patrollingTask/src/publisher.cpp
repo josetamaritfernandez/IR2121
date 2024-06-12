b4916c0f6e8efaa3d9eae259836c070f4cd329fc
josetamaritfernandez <144717796+josetamaritfernandez@users.noreply.github.com>
2024-06-12 19:44:59 +0200

    Patrolling V3 Comprobada (y aprobada)

--- patrollingTask/src/publisher.cpp
+++ patrollingTask/src/publisher.cpp
@@ -3,43 +3,103 @@
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 
-
 using namespace std::chrono_literals;
-double x,y,z,or_x,or_y,or_z,or_w, v_x,v_y,v_z,w_x,w_y,w_z;
-int t=0;
-void odom_callback(nav_msgs::msg::Odometry::SharedPtr msg){
+
+constexpr double x_start = -1;
+constexpr double y_start = 3;
+constexpr double z_start = 0;
+constexpr double w_start = 1;
+double v_x, w_z;
+int t = 0;
+
+void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) {
     v_x = msg->twist.twist.linear.x;
     w_z = msg->twist.twist.angular.z;
 }
-int main(int argc, char * argv[])
-{
+
+int main(int argc, char * argv[]) {
 rclcpp::init(argc, argv);
 auto node = rclcpp::Node::make_shared("publisher");
 auto publisher =
 node->create_publisher<geometry_msgs::msg::PoseStamped>("/goal_pose", 10);
 auto subscription =
-node->create_subscription<nav_msgs::msg::Odometry>("odom", 10,odom_callback);
+node->create_subscription<nav_msgs::msg::Odometry>("odom", 10,
+odom_callback);
+
 geometry_msgs::msg::PoseStamped message;
-nav_msgs::msg::Odometry odometry;
+
+message.pose.position.x = x_start;
+message.pose.position.y = y_start;
+message.pose.position.z = z_start;
+message.pose.orientation.x = 0;
+message.pose.orientation.y = 0;
+message.pose.orientation.z = 0;
+message.pose.orientation.w = w_start;
+
 rclcpp::WallRate loop_rate(500ms);
-rclcpp::WallRate poses(4);
-message.pose.position.x=-1;
-message.pose.position.y=3;
-message.pose.position.z=0;
-message.pose.orientation.x=0;
-message.pose.orientation.y=0;
-message.pose.orientation.z=0;
-message.pose.orientation.w=1;
+rclcpp::WallRate poses(3);
 
+std::vector<geometry_msgs::msg::PoseStamped> goals;
+goals.push_back(message); 
 
-while (v_x > 0.001 || w_z > 0.001 || t<=100) {
-    publisher->publish(message);
+geometry_msgs::msg::PoseStamped goal2;
+
+goal2.pose.position.x = 3 ;
+goal2.pose.position.y =4.5;
+goal2.pose.position.z = 0;
+goal2.pose.orientation.x = 0;
+goal2.pose.orientation.y = 0;
+goal2.pose.orientation.z = 0;
+goal2.pose.orientation.w = 1;
+goals.push_back(goal2); // 2
+
+geometry_msgs::msg::PoseStamped goal3;
+
+goal3.pose.position.x =8;
+goal3.pose.position.y = -1.5;
+goal3.pose.position.z = 0;
+goal3.pose.orientation.x = 0;
+goal3.pose.orientation.y = 0;
+goal3.pose.orientation.z = 0;
+goal3.pose.orientation.w = 1;
+goals.push_back(goal3); // 3
+
+geometry_msgs::msg::PoseStamped goal4;
+
+goal4.pose.position.x = -5 ;
+goal4.pose.position.y = -1.5 ;
+goal4.pose.position.z = 0;
+goal4.pose.orientation.x = 0;
+goal4.pose.orientation.y = 0;
+goal4.pose.orientation.z = 0;
+goal4.pose.orientation.w = 1;
+goals.push_back(goal4); //4
+
+geometry_msgs::msg::PoseStamped goal5;
+
+goal5.pose.position.x = 1;
+goal5.pose.position.y = 0;
+goal5.pose.position.z = 0;
+goal5.pose.orientation.x = 0;
+goal5.pose.orientation.y = 0;
+goal5.pose.orientation.z = 0;
+goal5.pose.orientation.w = 1;
+
+goals.push_back(goal5); //5
+size_t goal_index = 0;
+while (true) {
+    publisher->publish(goals[goal_index]);
     rclcpp::spin_some(node);
     loop_rate.sleep();
     t++;
-    if(v_x<0.005 && v_y<0.005 && t==100){
+    if (v_x < 0.005 && t >= 100)
+    {
         poses.sleep();
-        break;
+        goal_index++;
+        t = 0;
+        if (goal_index >= goals.size()) {
+            break;
+        }
     }
 }
 rclcpp::shutdown();
